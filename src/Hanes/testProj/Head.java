fe03a81f92e5b43a5347efbe36c71f0a84d73145
Andrew Hanes <amh7738@rit.edu>
2011-12-17 16:41:45 -0800

    Added class for the TextView at the top

---
+++ src/Hanes/testProj/Head.java
@@ -0,0 +1,14 @@
+package Hanes.testProj;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class Head extends TextView {
+	
+	String text = "Drink App";
+	public Head(Context context, Drink_Main dMain, Connector drinkServ) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
